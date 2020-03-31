0445e5de18aa3d32c3480ce54d2d5b38e65de482
Teddy <lunlun414@gmail.com>
2020-03-31 20:59:48 +0700

    refactor: logger

--- src/Context.js
+++ src/Context.js
@@ -25,10 +25,13 @@ let reducer = (state, action) => {
 }
 const logger = (reducer) => {
   const reducerWithLogger = (state, action) => {
+    let nextState = reducer(state, action);
+    console.group();
     console.log("%cPrevious State:", "color: #9E9E9E; font-weight: 700;", state);
     console.log("%cAction:", "color: #00A7F7; font-weight: 700;", action);
-    console.log("%cNext State:", "color: #47B04B; font-weight: 700;", reducer(state, action));
-    return reducer(state, action);
+    console.log("%cNext State:", "color: #47B04B; font-weight: 700;", nextState);
+    console.groupEnd();
+    return nextState;
   };
   return reducerWithLogger;
 }
